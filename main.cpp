bf9c2fdea74601b41a784aee7f03d87f5e35b04b
Dominick Girard <dominickgirard112@gmail.com>
2023-10-05 13:53:20 -0700

    the the code cave portion

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 
 DWORD ret_address = 0xCCAF90;
 
+DWORD* player_base;
+DWORD* game_base;
+DWORD* gold;
+
 __declspec(naked) void codecave() {
     __asm {
         pushad
     }
 
-    // code to modify gold
+    player_base = (DWORD*)0x017EED18;
+    game_base = (DWORD*)(*player_base + 0xA90);
+    gold = (DWORD*)(*game_base + 4);
+    *gold = 888;
 
     __asm {
         popad
