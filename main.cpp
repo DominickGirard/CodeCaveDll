4ecb3c6caf3300ba7fa5ad296c272497f3c8ca94
Dominick Girard <dominickgirard112@gmail.com>
2023-10-06 09:44:09 -0700

    added some comments to help understand the code

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@ DWORD* game_base;
 DWORD* gold;
 
 __declspec(naked) void codecave() {
+
+    
     __asm {
+        // this command is for making sure we save the registery value so we dont crash the game when jumping back to orignal part of the stack
         pushad
     }
 
@@ -17,7 +20,10 @@ __declspec(naked) void codecave() {
     *gold = 888;
 
     __asm {
+        // popad is the is restoring the registy from pushad
         popad
+
+        // these commands were the snippid we had to remove to get enough space for the jmp command we replaced it with
         mov eax, dword ptr ds : [ecx]
         lea esi, dword ptr ds : [esi]
         jmp ret_address
